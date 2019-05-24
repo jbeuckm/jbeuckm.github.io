c4fe66d3fe164166e1d1f65b36e126948a352533
j.beuckman <j.beuckman@klarna.com>
2019-05-24 15:03:31 +0200

    tempalte and another post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,11 +1,22 @@
 import React from "react"
 
+const styles = {
+  container: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    maxWidth: 600,
+  },
+}
+
 export default function Template({ data }) {
   const { markdownRemark: post } = data
 
   return (
-    <div>
+    <div style={styles.container}>
       <h1>{post.frontmatter.title}</h1>
+      <h3>{post.frontmatter.date}</h3>
+      <div>{post.frontmatter.tags.join(", ")}</div>
+
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
   )
